5c118e691edcd6b9ee3ebff619f55eeaf874888e
theroadof <1294911641@qq.com>
2020-07-23 20:47:21 +0800

    [GuessNumberGame][Teddy] feature: implement should_return_4A0B_when_play_guess_number_given_1234_and_1234

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -1,6 +1,10 @@
 package example;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class GuessNumberGame {
     private List<Integer> answer;
@@ -12,11 +16,29 @@ public class GuessNumberGame {
         this.answer = answerProvider.generateAnswer();
     }
 
-    public String guessNumber(List<Integer> answer) {
-        return null;
+    public String guessNumber(List<Integer> playerAnswer) {
+        return compare(this.answer,playerAnswer);
     }
 
     public List<Integer> getAnswer() {
         return answer;
+    }
+
+    private static String compare(List<Integer> answer,List<Integer> playerAnswer){
+        int A=0,B=0;
+        Map<Integer,Integer> playerAnswerMap = new HashMap<>();
+        for(int i=0;i<answer.size();i++){
+            playerAnswerMap.put(playerAnswer.get(i),i);
+        }
+        for(int i=0;i<answer.size();i++){
+            if(playerAnswerMap.containsKey(answer.get(i))){
+                if(playerAnswerMap.get(answer.get(i))==i){
+                    A++;
+                }else{
+                    B++;
+                }
+            }
+        }
+        return String.format("%dA%dB",A,B);
     }
 }
