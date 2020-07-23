8f99da06e6e1c820e01bf3512d8c64603bf8d696
theroadof <1294911641@qq.com>
2020-07-23 21:55:58 +0800

    [GuessNumberGame][Teddy] feature:implement should_generate_size_4_answer_when_play_generate_answer

--- src/main/java/example/AnswerProvider.java
+++ src/main/java/example/AnswerProvider.java
@@ -1,11 +1,21 @@
 package example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AnswerProvider implements AnswerGenerator {
     @Override
     public List<Integer> generateAnswer() {
-        return null;
+        List<Integer> answer = new ArrayList<>();
+        int size=0;
+        while (size<4){
+            int number = (int)(Math.random()*10);
+            if(!answer.contains(number)){
+                answer.add(number);
+                size++;
+            }
+        }
+        return answer;
     }
 
 }
--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -2,9 +2,7 @@ package example;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
