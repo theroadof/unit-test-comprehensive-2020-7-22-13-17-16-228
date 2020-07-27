d0f85c4c451941e88eaf9bafe0969db5fb8be8d8
theroadof <1294911641@qq.com>
2020-07-27 21:23:15 +0800

    [GuessNumberGame][Teddy]refactor:modify public to default

--- src/test/java/example/AnswerProviderTest.java
+++ src/test/java/example/AnswerProviderTest.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AnswerProviderTest {
+class AnswerProviderTest {
     @Test
     void should_generate_size_4_answer_when_play_generate_answer() {
 
